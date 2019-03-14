9b5f8ac2ae6213990201484130d5e9206dc11b1f
vagrant <vagrant@homestead>
2019-03-14 08:51:34 +0000

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 10vh;
+                margin: 0;
+                text-align:center
+            }
+            h1{
+                color: red;
+                font-size: 50px
+            }
+
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <h1>hello world!</h1>
+        </div>
+    </body>
+</html>
